96a3542cbc6c4e9b84d64a1714980888bcea5bc2
Amanuel <amanueleman@gmail.com>
2024-02-11 16:48:28 -0500

    Added a logic to the putDb method that accepts some content and adds it to the db.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,46 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  // Access the object store
+  const store = tx.objectStore("jate");
+  
+  // Add the content to the database
+  const request = store.put({ id: id, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+
+  // Wait for the transaction to complete.
+  await tx.done;
+
+  console.log('Data saved to the database', result);
+
+  // check if it has errors
+  console.error("putDb not implemented");
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error("getDb not implemented");
 
 initdb();
