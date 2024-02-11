48992073bfe16d13da91dd185e75dc98d270fa74
Amanuel <amanueleman@gmail.com>
2024-02-11 17:10:48 -0500

    Added handler fot the appinstalled event.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,33 +1,38 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    // Prevent Chrome 76 and later from showing the mini-infobar
-    event.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = event;
-    // Update UI notify the user they can add to home screen
-    butInstall.style.display = 'block';
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent Chrome 76 and later from showing the mini-infobar
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = event;
+  // Update UI notify the user they can add to home screen
+  butInstall.style.display = "block";
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    const { outcome } = await deferredPrompt.userChoice;
-    // Check the result
-    if (outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-    } else {
-        console.log('User dismissed the install prompt');
-    }
-    // We no longer need the prompt. Clear it up.
-    deferredPrompt = null;
-    // Hide the button
-    butInstall.style.display = 'none';
+butInstall.addEventListener("click", async () => {
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Check the result
+  if (outcome === "accepted") {
+    console.log("User accepted the install prompt");
+  } else {
+    console.log("User dismissed the install prompt");
+  }
+  // We no longer need the prompt. Clear it up.
+  deferredPrompt = null;
+  // Hide the button
+  butInstall.style.display = "none";
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("Jate has been installed!", event);
+
+  // Clear prompt
+  window.deferredPrompt = null;
+});
